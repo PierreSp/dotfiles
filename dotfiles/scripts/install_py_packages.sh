42bd17e0d7e03a6472fbffc0ea896156a40e8bde
Pierre <pierre.springer@tum.de>
2018-12-24 10:38:09 +0100

    Add structure to python install script

--- dotfiles/scripts/install_py_packages.sh
+++ dotfiles/scripts/install_py_packages.sh
@@ -1,2 +1,27 @@
-pip install --upgrade tqdm flake8 numpy matplotlib scipy scikit-learn epc importmagic seaborn pandas 
+#!/bin/sh
+
+# Tools for emacs
+pip install --upgrade flake8 importmagic yapf epc
 pip install --upgrade "jedi>=0.9.0" "json-rpc>=1.8.1" "service_factory>=0.1.5"
+
+# General
+pip install --upgrade numpy matplotlib seaborn pandas scipy tqdm jupyter ipython
+
+# Machine Learning
+echo "Do you want to install data science stuff?"
+echo "Y/N ?:"
+read answer
+echo Answer: $answer
+if [ "$answer" != "Y" ] || [ "$answer" != "y" ] || [ "$answer" != "Yes" ] || [ "$answer" != "yes" ]; then
+  pip install --upgrade scikit-learn tensorflow tensorly keras pytorch keras-tqdm statsmodels
+fi
+
+
+# Machine Learning
+echo "Do you want to install statistics stuff?"
+echo "Y/N ?:"
+read answer
+echo Answer: $answer
+if [ "$answer" != "Y" ] || [ "$answer" != "y" ] || [ "$answer" != "Yes" ] || [ "$answer" != "yes" ]; then
+  pip install --upgrade scikit-survival statsmodels PyMC
+fi
