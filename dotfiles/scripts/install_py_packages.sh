bf3bf1f165ffe4e7d72390e4923b6dddeaf02fa8
Pierre <pierre.springer@tum.de>
2018-12-24 12:18:46 +0100

    Add jupyterlab

--- dotfiles/scripts/install_py_packages.sh
+++ dotfiles/scripts/install_py_packages.sh
@@ -5,7 +5,7 @@ pip install --upgrade flake8 importmagic yapf epc
 pip install --upgrade "jedi>=0.9.0" "json-rpc>=1.8.1" "service_factory>=0.1.5"
 
 # General
-pip install --upgrade numpy matplotlib seaborn pandas scipy tqdm jupyter ipython
+pip install --upgrade numpy matplotlib seaborn pandas scipy tqdm jupyter ipython jupyterlab
 
 # Machine Learning
 echo "Do you want to install data science stuff?"
@@ -15,7 +15,6 @@ echo Answer: $answer
 if [ "$answer" != "Y" ] || [ "$answer" != "y" ] || [ "$answer" != "Yes" ] || [ "$answer" != "yes" ]; then
   pip install --upgrade scikit-learn tensorflow tensorly keras pytorch keras-tqdm statsmodels
 fi
-
 
 # Machine Learning
 echo "Do you want to install statistics stuff?"
