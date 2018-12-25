a788315e3b5ca173b13141c8157c403df431142e
Pierre <pierre.springer@tum.de>
2018-12-25 09:53:58 +0100

    Question for autocomplete, add autoflake and virtualfish

--- dotfiles/scripts/install_py_packages.sh
+++ dotfiles/scripts/install_py_packages.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
-
-# Tools for emacs
-pip install --upgrade flake8 importmagic yapf epc
-pip install --upgrade "jedi>=0.9.0" "json-rpc>=1.8.1" "service_factory>=0.1.5"
-
 # General
-pip install --upgrade numpy matplotlib seaborn pandas scipy tqdm jupyter ipython jupyterlab
+pip install --upgrade numpy matplotlib seaborn pandas scipy tqdm jupyter ipython jupyterlab virtualfish neovim
+
+# Emacs / autocomplete
+echo "Do you want to install python autocomplete setup?"
+echo "Y/N ?:"
+read answer
+echo Answer: $answer
+if [ "$answer" != "Y" ] || [ "$answer" != "y" ] || [ "$answer" != "Yes" ] || [ "$answer" != "yes" ]; then
+    pip install --upgrade autoflake flake8 importmagic yapf epc
+    pip install --upgrade "jedi>=0.9.0" "json-rpc>=1.8.1" "service_factory>=0.1.5"
+fi
 
 # Machine Learning
 echo "Do you want to install data science stuff?"
