634b57fd3a16abe55b32a54664c087d0a883badf
Pierre <pierre.springer@tum.de>
2019-01-16 12:34:05 +0100

    Refactoring

--- dotfiles/spacemacs.d/dot_org.el
+++ dotfiles/spacemacs.d/dot_org.el
@@ -1,45 +1,6 @@
-;; Google Calendar
-;; Nice way to get cal auth data (thanks to nathanael)
-;; Read lines from gcal.auth and store them in the variable
-;; Function to read lines of a file and output a list
-(defun read-lines (filePath)
-  "Return a list of lines of a file at filePath."
-  (with-temp-buffer
-    (insert-file-contents filePath)
-    (split-string (buffer-string) "\n" t)))
-(setq auth-lines (read-lines "~/gcal.auth"))
+;;;; General org config file
 
-(setq org-gcal-client-id (car auth-lines)
-      org-gcal-client-secret (car (cdr auth-lines)))
-(setq reftex-default-bibliography '("~/UNI/papers/references.bib"))
-
-;; see org-ref for use of these variables
-(setq org-ref-bibliography-notes "~/UNI/papers/notes.org"
-      org-ref-default-bibliography '("~/UNI/papers/references.bib")
-      org-ref-pdf-directory "~/UNI/papers/bibtex-pdfs/")
-
-;; Further variables for helm-bibtex
-(setq bibtex-completion-bibliography "~/UNI/papers/references.bib"
-      bibtex-completion-library-path "~/UNI/papers/bibtex-pdfs"
-      bibtex-completion-notes-path "~/UNI/papers/helm-bibtex-notes")
-
-;; open pdf with system pdf viewer (works on mac)
-(setq bibtex-completion-pdf-open-function
-      (lambda (fpath)
-        (start-process "open" "*open*" "open" fpath)))
-
-;; alternative
-;; (setq bibtex-completion-pdf-open-function 'org-open-file)
-
-;; Download directory
-(setq biblio-download-directory "~/UNI/papers/bibtex-pdfs")
-;; Hotkeys - thanks to nathbo
-(evil-leader/set-key-for-mode 'org-mode "I" 'interleave-mode)
-(evil-leader/set-key-for-mode 'org-mode "B" 'helm-bibtex)
-(evil-leader/set-key-for-mode 'bibtex-mode "B" 'helm-bibtex)
-;; Org mode keybinding
-(with-eval-after-load 'org
-  (evil-define-key '(normal insert) org-mode-map (kbd "M-RET M-RET") 'org-meta-return))
+;;;;;;;;;;;;;;;;ORG CONFIG
 
 ;; allow inline images
 (setq org-display-inline-images t)
@@ -116,3 +77,49 @@
               ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
               ("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
               ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
+
+;;;;Google calendar functions and org ref settings
+
+
+;; Google Calendar
+;; Nice way to get cal auth data (thanks to nathanael)
+;; Read lines from gcal.auth and store them in the variable
+;; Function to read lines of a file and output a list
+(defun read-lines (filePath)
+  "Return a list of lines of a file at filePath."
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (split-string (buffer-string) "\n" t)))
+(setq auth-lines (read-lines "~/gcal.auth"))
+
+(setq org-gcal-client-id (car auth-lines)
+      org-gcal-client-secret (car (cdr auth-lines)))
+(setq reftex-default-bibliography '("~/UNI/papers/references.bib"))
+
+;; see org-ref for use of these variables
+(setq org-ref-bibliography-notes "~/UNI/papers/notes.org"
+      org-ref-default-bibliography '("~/UNI/papers/references.bib")
+      org-ref-pdf-directory "~/UNI/papers/bibtex-pdfs/")
+
+;; Further variables for helm-bibtex
+(setq bibtex-completion-bibliography "~/UNI/papers/references.bib"
+      bibtex-completion-library-path "~/UNI/papers/bibtex-pdfs"
+      bibtex-completion-notes-path "~/UNI/papers/helm-bibtex-notes")
+
+;; open pdf with system pdf viewer (works on mac)
+(setq bibtex-completion-pdf-open-function
+      (lambda (fpath)
+        (start-process "open" "*open*" "open" fpath)))
+
+;; alternative
+;; (setq bibtex-completion-pdf-open-function 'org-open-file)
+
+;; Download directory
+(setq biblio-download-directory "~/UNI/papers/bibtex-pdfs")
+;; Hotkeys - thanks to nathbo
+(evil-leader/set-key-for-mode 'org-mode "I" 'interleave-mode)
+(evil-leader/set-key-for-mode 'org-mode "B" 'helm-bibtex)
+(evil-leader/set-key-for-mode 'bibtex-mode "B" 'helm-bibtex)
+;; Org mode keybinding
+(with-eval-after-load 'org
+  (evil-define-key '(normal insert) org-mode-map (kbd "M-RET M-RET") 'org-meta-return))
